098701db3cb12aaa4de502b20c5865b916dddb1f
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-09-19 13:56:08 +0530

    remove duplicate communities

--- src/components/home-community/Community.tsx
+++ src/components/home-community/Community.tsx
@@ -20,19 +20,6 @@ export default async function Community() {
           </div>
         </Link>
       ))}
-      {res.map((el) => (
-        <Link
-          className={styles.frameElement}
-          href={`/community/${el.name}`}
-          key={el.id}
-        >
-          <img src={el.img} alt={el.name} />
-          <div>
-            <img src={el.icon} alt={el.name} />
-            <span>{el.name}</span>
-          </div>
-        </Link>
-      ))}
     </Frame>
   );
 }
